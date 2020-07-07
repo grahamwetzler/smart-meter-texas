d789ac65173f41ccc7c7a804b449883d465297d3
Graham Wetzler <graham@wetzler.dev>
2020-07-07 14:20:23 -0500

    Remove support for Python 3.5
    Remove Python 3.5

--- setup.py
+++ setup.py
@@ -18,15 +18,19 @@ test_requirements = ['pytest>=3', ]
 
 setup(
     author="Graham Wetzler",
+<<<<<<< HEAD
     author_email='graham@wetzler.dev',
     python_requires='>=3.5',
+=======
+    author_email="graham@wetzler.dev",
+    python_requires=">=3.6",
+>>>>>>> f3589f5... Remove support for Python 3.5
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
