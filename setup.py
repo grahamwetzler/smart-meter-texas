cf6da9a61f0ba44c0a03021fc8f3efa6095704b7
Graham Wetzler <graham@wetzler.dev>
2020-07-07 17:18:37 -0500

    Apply Black formatting

--- setup.py
+++ setup.py
@@ -4,44 +4,48 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Graham Wetzler",
     author_email="graham@wetzler.dev",
     python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Package to connect to and retrieve data from the unoffical Smart Meter Texas API",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='smart_meter_texas',
-    name='smart_meter_texas',
-    packages=find_packages(include=['smart_meter_texas', 'smart_meter_texas.*']),
+    keywords="smart_meter_texas",
+    name="smart_meter_texas",
+    packages=find_packages(include=["smart_meter_texas", "smart_meter_texas.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/grahamwetzler/smart_meter_texas',
-    version='0.1.0',
+    url="https://github.com/grahamwetzler/smart_meter_texas",
+    version="0.1.0",
     zip_safe=False,
 )
--- smart_meter_texas/__init__.py
+++ smart_meter_texas/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Smart Meter Texas."""
 
 __author__ = """Graham Wetzler"""
-__email__ = 'graham@wetzler.dev'
-__version__ = '0.1.0'
+__email__ = "graham@wetzler.dev"
+__version__ = "0.1.0"
