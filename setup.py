7e1c93c2648e6ba6b0f37e67aa647421b5fe3ee3
Graham Wetzler <graham@wetzler.dev>
2020-07-09 17:23:13 -0500

    Add requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = ["aiohttp"]
+requirements = ["aiohttp==3.6.2", "python-dateutil==2.8.1"]
 
 setup_requirements = ["pytest-runner"]
 
