952ea888065c760a83d27593bec43f44c062a1b1
Graham Wetzler <graham@wetzler.dev>
2020-07-09 17:22:55 -0500

    Improve example

--- examples/get_meter_reading.py
+++ examples/get_meter_reading.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 import aiohttp
+import pytz
 
 from smart_meter_texas.async_api import Auth, Meter
 
@@ -10,16 +11,22 @@ password = os.environ["SMTPW"]
 meter_num = os.environ["SMTMETER"]
 esiid = os.environ["SMTESIID"]
 
+timezone = pytz.timezone("America/Chicago")
+
 
 async def main():
     async with aiohttp.ClientSession() as websession:
         auth = Auth(websession, username, password)
+
+        print("Authenicating...")
         await auth.authenticate()
 
+        print("Reading meter...")
         meter = Meter(auth, esiid, meter_num)
         await meter.async_read_meter()
 
-        print(f"{meter.reading} kW @ {meter.reading_datetime}")
+        localized_time = meter.reading_datetime.astimezone(timezone)
+        print(f"{meter.reading:,.0f} kW @ {localized_time}")
 
 
 if __name__ == "__main__":
