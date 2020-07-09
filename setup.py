25f3b426ab07c9d38b5cb6bb325acf5163befd38
Graham Wetzler <graham@wetzler.dev>
2020-07-09 10:11:21 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/grahamwetzler/smart-meter-texas",
-    version="0.1.0",
+    version="version='0.2.0'",
     zip_safe=False,
 )
--- smart_meter_texas/__init__.py
+++ smart_meter_texas/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Graham Wetzler"""
 __email__ = "graham@wetzler.dev"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.2.0'"
