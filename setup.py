081fadc46f543e6cd26e6ca16c2891fbbda38a1d
Graham Wetzler <graham@wetzler.dev>
2020-07-09 15:54:26 -0500

    Remove history from description

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
 requirements = []
 
 setup_requirements = ["pytest-runner"]
@@ -34,7 +31,7 @@ setup(
         Smart Meter Texas API",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="smart_meter_texas",
     name="smart_meter_texas",
