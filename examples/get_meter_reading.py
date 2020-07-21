f190527fbbed2e7e5838564988bb26d45fb32d89
Graham Wetzler <graham@wetzler.dev>
2020-07-21 17:37:40 -0500

    Refactor into a single class

--- examples/get_meter_reading.py
+++ examples/get_meter_reading.py
@@ -4,7 +4,7 @@ import os
 import aiohttp
 import pytz
 
-from smart_meter_texas.async_api import Auth, Meter
+from smart_meter_texas.async_api import SMTMeterReader
 
 username = os.environ["SMTUSER"]
 password = os.environ["SMTPW"]
@@ -14,19 +14,18 @@ timezone = pytz.timezone("America/Chicago")
 
 async def main():
     async with aiohttp.ClientSession() as websession:
-        auth = Auth(websession, username, password)
+        meter = SMTMeterReader(websession, username, password)
 
         print("Authenicating...")
-        await auth.authenticate()
+        await meter.authenticate()
 
-        meter = Meter(auth)
         await meter.read_dashboard()
         print(f"Meter: {meter.meter}")
         print(f"ESIID: {meter.esiid}")
         print(f"Address: {meter.address}")
 
         print("Reading meter (takes about 30s)...")
-        await meter.async_read_meter()
+        await meter.read_meter()
 
         localized_time = meter.reading_datetime.astimezone(timezone)
         print(f"{meter.reading:,.0f} kW @ {localized_time}")
--- smart_meter_texas/async_api.py
+++ smart_meter_texas/async_api.py
@@ -3,10 +3,9 @@ import datetime
 import logging
 
 import dateutil.parser
-from aiohttp import ClientResponse, ClientSession
+from aiohttp import ClientResponse, ClientSession, ClientResponseError
 
 URL = "https://www.smartmetertexas.com/"
-DEFAULT_TIMEOUT = 15
 ON_DEMAND_READ_RETRY_TIME = 15
 USER_AGENT = (
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14;"
@@ -16,34 +15,55 @@ USER_AGENT = (
 _LOGGER = logging.getLogger(__name__)
 
 
-class Auth:
+class SMTMeterReader:
     def __init__(
-        self,
-        websession: ClientSession,
-        username: str,
-        password: str,
-        default_timeout: int = DEFAULT_TIMEOUT,
+        self, websession: ClientSession, username: str, password: str,
     ) -> None:
         self.websession = websession
         self.username = username
         self.password = password
-        self.default_timeout = default_timeout
+        self.authenticated = False
         self.headers = {
             "User-Agent": USER_AGENT,
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
+        self.esiid = None
+        self.meter = None
+        self._address = None
+        self._reading_data = None
+
+    async def _api_request(
+        self, websession: ClientSession, path: str = "", method: str = "post", **kwargs,
+    ) -> ClientResponse:
+        try:
+            return await websession.request(
+                method,
+                f"{URL}{path}",
+                json=kwargs.get("json"),
+                headers=kwargs.get("headers"),
+            )
+        except ClientResponseError as e:
+            _LOGGER.warning("Server responded error code %s" % e.status)
+            if e.status == 401:
+                self.authenticated = False
 
     async def _set_token(self, token: str) -> None:
         self.headers["Authorization"] = f"Bearer {token}"
 
+    async def _get_dashboard(self) -> ClientResponse.json:
+        resp = await self._api_request(
+            self.websession, "api/dashboard", headers=self.headers,
+        )
+        return await resp.json()
+
     async def authenticate(self) -> ClientSession:
 
         # Make an inital GET request otherwise subsequent calls will timeout
-        await api_request(
+        await self._api_request(
             self.websession, method="get", headers=self.headers,
         )
-        resp = await api_request(
+        resp = await self._api_request(
             self.websession,
             "api/user/authenticate",
             json={
@@ -63,24 +83,9 @@ class Auth:
             raise SMTError
 
         await self._set_token(json_response["token"])
+        self.authenticated = True
 
         return self.websession
-
-
-class Meter:
-    """Class representation of a smart meter."""
-
-    def __init__(self, auth: Auth, esiid: str = None, meter: str = None) -> None:
-        self.auth = auth.websession
-        self.headers = auth.headers
-        self.esiid = esiid
-        self.meter = meter
-        self._address = None
-        self._reading_data = None
-
-    async def _get_dashboard(self) -> ClientResponse.json:
-        resp = await api_request(self.auth, "api/dashboard", headers=self.headers,)
-        return await resp.json()
 
     async def read_dashboard(self) -> None:
         resp = await self._get_dashboard()
@@ -92,16 +97,16 @@ class Meter:
         self.meter = meter_details.get("meterNumber")
         self.esiid = meter_details.get("esiid")
 
-    async def async_read_meter(self) -> None:
-        await api_request(
-            self.auth,
+    async def read_meter(self) -> None:
+        await self._api_request(
+            self.websession,
             "/api/ondemandread",
             json={"ESIID": self.esiid, "MeterNumber": self.meter},
             headers=self.headers,
         )
         while True:
-            reading = await api_request(
-                self.auth,
+            reading = await self._api_request(
+                self.websession,
                 "api/usage/latestodrread",
                 json={"ESIID": self.esiid},
                 headers=self.headers,
@@ -141,19 +146,3 @@ class Meter:
 
 class SMTError(Exception):
     pass
-
-
-async def api_request(
-    websession: ClientSession,
-    path: str = "",
-    method: str = "post",
-    timeout: int = DEFAULT_TIMEOUT,
-    **kwargs,
-) -> ClientResponse.json:
-    return await websession.request(
-        method,
-        f"{URL}{path}",
-        json=kwargs.get("json"),
-        headers=kwargs.get("headers"),
-        timeout=timeout,
-    )
