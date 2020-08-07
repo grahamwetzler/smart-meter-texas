671e43ac271e408d3b5a8f2de58776f371850008
Graham Wetzler <graham@wetzler.dev>
2020-08-07 17:14:01 -0500

    Add exception docstrings

--- smart_meter_texas/exceptions.py
+++ smart_meter_texas/exceptions.py
@@ -1,14 +1,23 @@
 class SmartMeterTexasException(Exception):
+    """Base exception for more specific exceptions to inherit from."""
+
     ...
 
 
 class SmartMeterTexasAuthError(SmartMeterTexasException):
+    """Exception for authentication failures.
+    Either wrong username or wrong password."""
+
     ...
 
 
 class SmartMeterTexasAuthExpired(SmartMeterTexasException):
+    """Exception for when a token is no longer valid."""
+
     ...
 
 
 class SmartMeterTexasAPIError(SmartMeterTexasException):
+    """General exception for unknown API responses."""
+
     ...
