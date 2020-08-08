28e6e59d932641c4247bb20e9b21a56a5a2c093b
Graham Wetzler <graham@wetzler.dev>
2020-08-08 09:32:54 -0500

    Bump version: 0.4.0 → 0.4.1

--- smart_meter_texas/__init__.py
+++ smart_meter_texas/__init__.py
@@ -28,7 +28,7 @@ from .exceptions import (
 
 __author__ = "Graham Wetzler"
 __email__ = "graham@wetzler.dev"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 _LOGGER = logging.getLogger(__name__)
